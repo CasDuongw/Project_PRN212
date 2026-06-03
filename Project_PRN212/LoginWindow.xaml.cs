f62f6a2246cf6275db4b00e069606422ffb98b87
Vương Huỳnh <fakevuonghuynh45@gmail.com>
2026-06-03 22:07:38 +0700

    App.xaml modified

---
+++ Project_PRN212/LoginWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_PRN212
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string Username = txtUsername.Text;
+            string Password = txtPassword.Password;
+            // NẾU NHẬP ĐÚNG THÌ CHO VÀO WINDOW MAIN
+            if (Username == "admin" && Password == "admin")
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu!",
+                    "Lỗi đăng nhập", 
+                    MessageBoxButton.OK, 
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("BẠN CÓ MUỐN THOÁT KHÔNG?",
+                "CẢNH BÁO", MessageBoxButton.YesNo,
+                MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                // THOÁT ỨNG DỤNG
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
